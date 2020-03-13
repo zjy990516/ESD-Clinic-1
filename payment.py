faf7faee327e23b26dcd5de33b7ce030fc19c5f4
Zhang-yiling <54891648+Zhang-yiling@users.noreply.github.com>
2020-03-13 16:58:10 +0800

    payment.py
    This is payment microservice integrating Paypal API

---
+++ payment.py
@@ -0,0 +1,138 @@
+import paypalrestsdk
+import logging
+from paypalrestsdk import Payout, ResourceNotFound
+
+
+paypalrestsdk.configure({
+  "mode": "sandbox", # sandbox or live
+  "client_id": "EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM",
+  "client_secret": "EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM" })
+
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+from datetime import datetime
+import json
+import pika
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/payment'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db=SQLAlchemy(app)
+CORS(app)
+
+db=SQLAlchemy(app)
+
+#communication patterns:
+
+
+
+class Payment(db.MOdel):
+    __tablename__ = 'payment'
+    payment_id = db.Column(db.Integer, primary_key=True)
+    treatment_id = db.Column(db.Integer, db.ForeignKey('treatment_id'),nullable=False)
+    payment_date = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    payment_status=db.Column(db.String(10),nullable=False,default="incompleted")
+    price=db.Column(db.Float,nullable=False)
+
+    
+    def json(self):
+        dto={
+            'payment_id':self.payment_id,
+            'treatment_id':self.treatment_id,
+            'price':self.price,
+            'payment_date':self.payment_date,
+            'payment_status':self.payment_status,
+
+        }
+        return dto
+#get data posted by treatment
+
+
+@app.route("/payment_id")
+def get_all():
+    return {'payments:':[payment.json() for payment in Payment.query.all() ]}
+
+@app.route("/payment/paymemt_id")
+def find_payment_by_id(payment_id):
+    payment=payment.query.filter_by(payment_id=payment_id).first()
+    if payment:
+        return payment.json()
+
+#generate order(implementing paypal API)
+@app.route("payment/payment_id,method=[POST]")
+def create_payment():
+    treatment_id=request.json['treatment_id']
+    price=request.json['price']
+    status=201
+    result={}
+
+    #retrieve information  about payment and payment items from the request
+    
+    payment = Payment(treatment_id=treatment_id,price=price)
+    
+    paypalrestsdk.configure({
+        "mode": "sandbox", # sandbox or live
+        "client_id": "ARiPc1IIjlwxqkCAFyBsMf8T5Z6YsxjDmU_IEmHiS8kPYw_hBt5PhzDlDqyHhI5DoYlxSWvZkWyQrLBI",
+        "client_secret": "EF8H6qOQ0ZxCU3BlpuJj8roZyUDalYjtsaB6EMC1rFNZ3drIYhNTRPW4kzFP5p8Q7Hb_eFgKupVNSERq" })
+
+    payment = paypalrestsdk.Payment({
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"},
+        "redirect_urls": {
+            "return_url": "http://localhost:3000/payment/execute",
+            "cancel_url": "http://localhost:3000/"},
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "treatment": treatment_id,
+                    "sku": "item",
+                    "price": price,
+                    "currency": "SGD",
+                    }]},
+            "amount": {
+                "total": price,
+                "currency": "SGD"},
+            "description": treatment.description}]})
+
+    if payment.create():
+        try:
+            #authorize the payment
+            for link in payment.links:
+                if link.rel=='approval_url':
+                    if link.rel == "approval_url":
+                # Convert to str to avoid google appengine unicode issue
+                # https://github.com/paypal/rest-api-sdk-python/pull/58
+                        approval_url = str(link.href)
+                        print("Redirect for approval: %s" % (approval_url))
+                        db.session.add(payment)
+                        db.session.commit()
+        except Exception as e:
+            status=500
+    else:
+        print(payment.error)
+        result = {'status':500, "message":"An error occurred when creating the order in DB", "error":str(payment.error)}
+
+       
+def execute_payment(payment_id):
+    payment = paypalrestsdk.Payment.find(payment_id)
+    result={}
+    #match payer id with per parent?
+    if payment.execute({"payer_id":payer_id}):
+        print("Payment execute successfully")
+        result={'status':200,.}
+    else:
+        print(payment.error) # Error Hash
+
+@app.route('/payment/<int:payment_id>',methods=['PUT'])
+def update_payment_status(payment_id):
+    payment=payment.
+    
+
+ 
+ 
+
+
